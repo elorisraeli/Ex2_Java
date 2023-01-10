f5cd5525564c899c19c0e56c7ed427146ce94a78
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2023-01-10 22:46:11 +0200

    fix getMaxPriority things

--- src/CustomExecutor.java
+++ src/CustomExecutor.java
@@ -1,4 +1,3 @@
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.TreeSet;
 import java.util.concurrent.*;
@@ -7,29 +6,38 @@ import java.util.concurrent.*;
  * A CustomExecutor is a class that implements the Executor interface and has a priority queue
  * and a constructor that sets the priority queue
  */
-public class CustomExecutor  extends ThreadPoolExecutor {
+public class CustomExecutor extends ThreadPoolExecutor {
 
-    private int currentMaxPriority;// the current max priority of the tasks in the queue
+    private final int PRIORITY_MIN_OPTION = 10;
     private HashMap<Integer, Integer> priorityHashMap = new HashMap<>();
+
+    public void setPriorityHashMap() {
+        for (int i = 1; i <= PRIORITY_MIN_OPTION; i++) {
+            this.priorityHashMap.put(i, 0);
+        }
+    }
 
     /**
      * Creates a new CustomExecutor that will, upon running, execute the given
      */
     public CustomExecutor() {
-        super(Runtime.getRuntime().availableProcessors()/2,
-                Runtime.getRuntime().availableProcessors()-1,
+        super(Runtime.getRuntime().availableProcessors() / 2,
+                Runtime.getRuntime().availableProcessors() - 1,
                 300L, TimeUnit.MILLISECONDS,
                 new PriorityBlockingQueue<>(new TreeSet<>(new CustomExecutorComparator())));
+
+        setPriorityHashMap();
     }
 
 
     /**
-     *  submits a task to the executor and adds it to the priority queue
+     * submits a task to the executor and adds it to the priority queue
+     *
      * @param callable the callable task to be executed
      * @param taskType the type of the task
      * @return the Task that was submitted
      */
-    public <T> Future<T> submit(Callable<T> callable, TaskType taskType){
+    public <T> Future<T> submit(Callable<T> callable, TaskType taskType) {
         if (callable == null) {
             throw new NullPointerException("Callable is null");
         }
@@ -37,17 +45,15 @@ public class CustomExecutor  extends ThreadPoolExecutor {
             throw new NullPointerException("TaskType is null");
         }
         Task<T> task = Task.createTask(callable, taskType);
+        this.priorityHashMap.put(task.getPriority(), this.priorityHashMap.get(task.getPriority()) + 1);
         MyFutureTask<T> myFutureTask = new MyFutureTask<T>(callable, taskType.getPriorityValue());
-        if(task.getPriority() < currentMaxPriority){
-            currentMaxPriority = task.getPriority();
-        }
-        priorityHashMap.put(taskType.getPriorityValue(), priorityHashMap.getOrDefault(taskType.getPriorityValue(), 0) + 1);
-        execute(myFutureTask);
+        super.execute(myFutureTask);
         return myFutureTask;
     }
 
     /**
      * submits a task to the executor and adds it to the priority queue
+     *
      * @param callable the task to submit
      * @return the Task that was submitted
      */
@@ -56,12 +62,9 @@ public class CustomExecutor  extends ThreadPoolExecutor {
             throw new NullPointerException("Callable is null");
         }
         Task<T> task = Task.createTask(callable);
+        this.priorityHashMap.put(task.getPriority(), this.priorityHashMap.get(task.getPriority()) + 1);
         MyFutureTask<T> myFutureTask = new MyFutureTask<T>(callable, task.getPriority());
-        if(task.getPriority() < currentMaxPriority){
-            currentMaxPriority = task.getPriority();
-        }
-        priorityHashMap.put(task.getPriority(), priorityHashMap.getOrDefault(task.getPriority(), 0) + 1);
-        execute(myFutureTask);
+        super.execute(myFutureTask);
         return myFutureTask;
     }
 
@@ -70,17 +73,13 @@ public class CustomExecutor  extends ThreadPoolExecutor {
      * before executing a task, checks if the task's priority is higher than the current max priority
      * if it is, the current max priority is updated
      * @param r the task to be executed
-     *          @param t the thread that will execute the task
+     * @param t the thread that will execute the task
      *
      */
     protected void beforeExecute(Thread t, Runnable r) {
         MyFutureTask myFutureTask = (MyFutureTask) r;
-     priorityHashMap.put(myFutureTask.getPriority(), priorityHashMap.get(myFutureTask.getPriority()) - 1);
-        if (myFutureTask.getPriority() > currentMaxPriority) {
-            currentMaxPriority = myFutureTask.getPriority();
-        }
+        this.priorityHashMap.put(myFutureTask.getPriority(), this.priorityHashMap.get(myFutureTask.getPriority()) - 1);
     }
-
 
     /**
      * this method shuts down the executor and waits for all the tasks to finish
@@ -96,10 +95,17 @@ public class CustomExecutor  extends ThreadPoolExecutor {
 
     /**
      * function to get the current max priority of the tasks in the queue
+     *
      * @return the current max priority of the tasks in the queue
      */
     public int getCurrentMax() {
-        return this.currentMaxPriority;
+//        System.out.println(priorityHashMap.toString());
+        for (int i = 1; i < PRIORITY_MIN_OPTION; i++) {
+            if (priorityHashMap.get(i) != null && priorityHashMap.get(i) > 0) {
+                return i;
+            }
+        }
+        return PRIORITY_MIN_OPTION;
     }
 
 
