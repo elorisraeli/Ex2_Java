0b7b0954fa84d338babdde8685a55d0bca952861
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2023-01-10 22:47:23 +0200

    add new tests

--- src/Tests.java
+++ src/Tests.java
@@ -2,10 +2,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
@@ -48,9 +47,55 @@ public class Tests {
             throw new RuntimeException(e);
         }
         logger.info(()-> "Reversed String = " + reversed);
-        logger.info(()->String.valueOf("Total Price = " + totalPrice));
+        logger.info(()-> String.valueOf("Total Price = " + totalPrice));
         logger.info(()-> "Current maximum priority = " +
                 customExecutor.getCurrentMax());
         customExecutor.gracefullyTerminate();
     }
+
+    @Test
+    public void anotherTests() throws ExecutionException, InterruptedException {
+        CustomExecutor customExecutor = new CustomExecutor();
+
+        Task<String> taskString = Task.createTask(()->{
+            StringBuilder sb = new StringBuilder("Hello ");
+            for (char i = 'a'; i < 't'; i++) {
+                sb.append(i);
+            }
+            return sb.toString();
+        }, TaskType.IO);
+        Future<String> futureString = customExecutor.submit(taskString);
+
+        Task<Integer> task = Task.createTask(()->{
+            // calculate the factorial of 5
+            int sum = 1;
+            for (int i = 1; i <= 5; i++) {
+                sum *= i; //1*1=1, 1*2=2, 2*3=6, 6*4=24, 24*5=120
+            }
+            return sum;
+        }, TaskType.COMPUTATIONAL);
+        Future<Integer> futureTask = customExecutor.submit(task);
+
+        assertEquals(futureTask.get(), 120);
+        assertEquals("Hello abcdefghijklmnopqrs", futureString.get());
+
+        try {
+            Integer result = futureTask.get();
+            System.out.println("Result: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            String result = futureString.get();
+            System.out.println("Result: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
 }
